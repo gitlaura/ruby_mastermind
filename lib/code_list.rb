55f227aad361d7735f4284a5c11a13cf0b63869d
Laura Rokita <laura.rokita@gmail.com>
2014-07-03 11:12:48 -0500

    refactor update list

--- lib/code_list.rb
+++ lib/code_list.rb
@@ -1,5 +1,5 @@
 class CodeList
-	attr_accessor :possibilities
+	attr_accessor :possibilities, :temp_code, :temp_guess, :new_possibilities
 	attr_reader :colors
 
 	def initialize(colors)
@@ -16,45 +16,50 @@ class CodeList
 		end
 	end
 
-	def update_list(black, white, guess)		
+	def update_list(black, white, guess)
+		@new_possibilities = []
+		@saved_guess = guess.to_sym
 		index = 0
-		saved_guess = guess.to_sym
 
-		until index == @possibilities.size do
-			blackcounter, whitecounter = 0, 0
-			temp_guess = saved_guess.to_s
-			saved_code = @possibilities[index].to_sym
-			temp_code = saved_code.to_s
-
-			char = 0
-			until char == 4 do 
-				if temp_code[char] == temp_guess[char]
-					blackcounter += 1
-					temp_code[char] = "X"
-					temp_guess[char] = "X"
-				end
-				char += 1
-			end
-
-			@colors.each do |color|
-				temp_code_color_count = temp_code.count(color)
-				temp_guess_color_count = temp_guess.count(color)
-				if temp_code_color_count == 0 || temp_guess_color_count == 0
-					whitecounter = whitecounter
-				elsif temp_code_color_count == temp_guess_color_count
-					whitecounter += temp_code_color_count
-				else
-					whitecounter += [temp_code_color_count, temp_guess_color_count].min
-				end
-			end
-
-			if blackcounter != black || whitecounter != white
-				@possibilities[index] = "INVALID" 
-			end
-
-			index += 1	
+		until index == @possibilities.size do 
+			@saved_code = @possibilities[index].to_sym
+			@temp_guess, @temp_code = @saved_guess.to_s, @saved_code.to_s
+			blackcounter = count_black_pegs
+			whitecounter = count_white_pegs
+			update_new_possibilities(black, blackcounter, white, whitecounter, index)
+			index += 1
 		end
 
+		@possibilities = @new_possibilities
+	end
+
+	def count_black_pegs
+		blackcounter = 0
+		(0..3).each do |index|
+			if @temp_code[index] == @temp_guess[index]
+				blackcounter += 1
+				@temp_code[index] = "X"
+				@temp_guess[index] = "X"
+			end
+		end
+		blackcounter
+	end
+
+	def count_white_pegs
+		whitecounter = 0
+		@colors.each do |color|
+			whitecounter += [@temp_code.count(color), @temp_guess.count(color)].min
+		end
+		whitecounter
+	end
+
+	def update_new_possibilities(black, blackcounter, white, whitecounter, index)
+		if blackcounter == black && whitecounter == white
+			@new_possibilities << @possibilities[index]
+		end
+	end
+
+	def delete_invalid_possibilities
 		@possibilities.delete("INVALID")
 		@possibilities
 	end
--- spec/code_list_spec.rb
+++ spec/code_list_spec.rb
@@ -11,9 +11,41 @@ describe "Code List" do
 		expect(@code_list.possibilities.size).to eq(1296)
 	end
 
-	it "updates based on the number of black and white pegs it's given" do 
-		@code_list.update_list(0, 2, "GOYR")
-		expect(@code_list.possibilities).to include("PYRY")
-		expect(@code_list.possibilities).not_to include("GPBP")
+	it "updates the possibilities list" do 
+		@code_list.possibilities = ["RRRR", "PROY", "GGRR", "PGRO"]
+		@code_list.update_list(2, 1, "PRPO")
+		expect(@code_list.possibilities).to eq(["PROY", "PGRO"])
+	end
+
+	it "counts the black pegs when comparing the temp_code versus the temp_guess" do 
+		@code_list.temp_guess, @code_list.temp_code = "RRPP", "PORY"
+		expect(@code_list.count_black_pegs).to eq(0)
+		@code_list.temp_guess, @code_list.temp_code = "RRPP", "OROP"
+		expect(@code_list.count_black_pegs).to eq(2)
+	end	
+
+	it "counts the white pegs when comparing the temp_code versus the temp_guess" do 
+		@code_list.temp_guess, @code_list.temp_code = "RRPP", "PORY"
+		expect(@code_list.count_white_pegs).to eq(2)
+		@code_list.temp_guess, @code_list.temp_code = "RRPP", "OOOO"
+		expect(@code_list.count_white_pegs).to eq(0)
+	end	
+
+	it "adds code to the new possibilities list" do 
+		@code_list.possibilities = ["RRRR", "RYPO", "GGRR"]
+		@code_list.new_possibilities = []
+		@code_list.update_new_possibilities(2, 2, 1, 1, 1)
+		expect(@code_list.new_possibilities).to eq(["RYPO"])
+
+		@code_list.possibilities = ["RRRR", "RYPO", "GGRR"]
+		@code_list.new_possibilities = []
+		@code_list.update_new_possibilities(2, 0, 3, 3, 1)
+		expect(@code_list.new_possibilities).to eq([])
+	end	
+
+	it "deletes invalid possibilities" do 
+		@code_list.possibilities = ["RYPP", "INVALID", "RPPP", "INVALID"]
+		@code_list.delete_invalid_possibilities
+		expect(@code_list.possibilities).to eq(["RYPP", "RPPP"])
 	end
 end
